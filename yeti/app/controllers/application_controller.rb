c1628fb46ed963f01257c52d153b04bab16dc66d
StevenDRiggs <sfriggs21@gmail.com>
2020-07-16 20:28:02 -0600

    completed action templates

--- yeti/app/controllers/application_controller.rb
+++ yeti/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def define_variables
     @obj = Object.const_get(self.class.to_s[0..-11].singularize)
-    @snake_case = @obj.class.to_s.underscore
+    @snake_case = @obj.to_s.underscore
   end
 
   def index
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
       self.instance_variable_set("@#{@snake_case}", @obj.find(params[:id]))
     rescue ActiveRecord::RecordNotFound
       flash[:errors] = ["#{@obj.name} not found"]
-      redirect_to self.send("#{@snake_case.pluralize}")
+      redirect_to self.send("#{@snake_case.pluralize}_path")
     end
   end
 
@@ -24,17 +24,58 @@ class ApplicationController < ActionController::Base
   end
 
   def create
+    object_ = @obj.new(self.object_params)
+    if object_.save
+      redirect_to object_
+    else
+      flash[:errors] = object_.errors.full_messages
+      render self.send("new_#{@snake_case}_path")
+    end
   end
 
   def edit
+    begin
+      self.instance_variable_set("@#{@snake_case}", @obj.find(params[:id]))
+    rescue ActiveRecord::RecordNotFound
+      flash[:errors] = ["#{@obj.name} not found"]
+      redirect_to self.send("#{@snake_case.pluralize}_path")
+    end
   end
 
   def update
-  end
-
-  def destroy
+    begin
+      object_ = self.instance_variable_set("@#{@snake_case}", @obj.find(params[:id]))
+      if object_.update(self.object_params)
+        redirect_to object_
+      else
+        flash[:errors] = object_.errors.full_messages
+        render self.send("edit_#{@snake_case}_path")
+      end
+    rescue ActiveRecord::RecordNotFound
+      flash[:errors] = ["#{@obj.name} not found"]
+      redirect_to self.send("#{@snake_case.pluralize}_path")
+    end
   end
 
   def delete
+    begin
+      object_ = self.instance_variable_set("@#{@snake_case}", @obj.find(params[:id]))
+    rescue ActiveRecord::RecordNotFound
+      flash[:errors] = ["#{@obj.name} not found"]
+      redirect_to self.send("#{@snake_case.pluralize}_path")
+    end
+  end
+
+  def destroy
+    begin
+      object_ = @obj.find(params[:id]))
+
+      object_.destroy
+
+      redirect_to self.send("#{@snake_case.pluralize}_path")
+    rescue ActiveRecord::RecordNotFound
+      flash[:errors] = ["#{@obj.name} not found"]
+      redirect_to self.send("#{@snake_case.pluralize}_path")
+    end
   end
 end
--- yeti/app/controllers/books_controller.rb
+++ yeti/app/controllers/books_controller.rb
@@ -1,2 +1,6 @@
 class BooksController < ApplicationController
+  protected
+    def object_params
+      params.require(:book).permit([:title, :synopsis])
+    end
 end
