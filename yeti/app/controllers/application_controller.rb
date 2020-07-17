0dd03f29e51bf18bcdc215ec00f3a2ef322ffaa8
StevenDRiggs <sfriggs21@gmail.com>
2020-07-17 06:11:41 -0600

    continue helper methods for partial templates

--- yeti/app/controllers/application_controller.rb
+++ yeti/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def index
-    self.instance_variable_set("@#{@snake_case}", @obj.all)
+    self.instance_variable_set("@#{@snake_case.pluralize}", @obj.all)
   end
 
   def show
--- yeti/app/helpers/application_helper.rb
+++ yeti/app/helpers/application_helper.rb
@@ -6,4 +6,30 @@ module ApplicationHelper
   def destroy_path(object_)
     self.send("destroy_#{object_.class_name.underscore}_path", object_.id)
   end
+
+  #######
+
+  def display_attributes(object_)
+    attrs = object_.attributes
+    attrs.delete_if do |key, val|
+      %w(created_at updated_at id).include? (key) or val == object_.display_name
+    end
+
+    attrs.collect do |key, val|
+      <<-HTML
+        <h3>#{key}</h3>
+        <p>#{val || 'Not provided'}</p>
+      HTML
+    end.join('<br>').html_safe
+  end
+
+  def index(group, objects)
+    html = "<h2>#{group}</h2>"
+
+    objects.each do |object_|
+      html << "<h3>#{object_.display_name}</h3>"
+    end
+
+    html.html_safe
+  end
 end
